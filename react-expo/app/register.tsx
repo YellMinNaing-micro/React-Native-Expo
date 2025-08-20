76200c7d11f0f464124badb94181341b8d38b54e
Yell Min Naing <yellminnaing5@gmail.com>
2025-08-20 16:40:08 +0630

    Update register.tsx

--- react-expo/app/register.tsx
+++ react-expo/app/register.tsx
@@ -9,18 +9,19 @@ import {
     VStack
 } from "@gluestack-ui/themed";
 import { useRouter } from "expo-router";
-import { TouchableOpacity, KeyboardAvoidingView, Platform } from "react-native";
+import { TouchableOpacity, KeyboardAvoidingView, Platform, Keyboard, TouchableWithoutFeedback, StyleSheet } from "react-native";
 import { StatusBar } from "expo-status-bar";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function RegisterScreen() {
     const router = useRouter();
     const [username, setUsername] = useState("");
+    const [fullName, setFullName] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [showPassword, setShowPassword] = useState(false);
 
     const handleRegister = () => {
-        // Implement your registration logic here
-        // Using a custom message instead of a native alert
         console.warn(`Account created for ${username}`);
         router.replace("/");
     };
@@ -31,67 +32,94 @@ export default function RegisterScreen() {
             behavior={Platform.OS === "ios" ? "padding" : "height"}
         >
             <StatusBar style="dark" />
-            <Box flex={1} justifyContent="center" alignItems="center"  p="$5">
-                <VStack space="md" w="90%" alignItems="flex-start">
-                    <Text size="3xl" fontWeight="bold" color="$black">
-                        Sign Up
-                    </Text>
-                    <Text size="md" color="$coolGray500">
-                        Start Your Journey.
-                    </Text>
-                    <Text size="md" color="$black">
-                        User Name
-                    </Text>
-                    <Input w="100%" size="lg" rounded="$2xl">
-                        <InputField
-                            placeholder="Full name"
-                            value={username}
-                            onChangeText={setUsername}
-                        />
-                    </Input>
-                    <Text size="md" color="$black">
-                        Email address
-                    </Text>
-                    <Input w="100%" size="lg" rounded="$2xl">
-                        <InputField
-                            placeholder="Email"
-                            keyboardType="email-address"
-                            value={email}
-                            onChangeText={setEmail}
-                        />
-                    </Input>
-                    <Text size="md" color="$black">
-                        Password
-                    </Text>
-                    <Input w="100%" size="lg" rounded="$2xl">
-                        <InputField
-                            placeholder="Password"
-                            secureTextEntry
-                            value={password}
-                            onChangeText={setPassword}
-                        />
-                    </Input>
-
-                    <Button
-                        w="100%"
-                        size="lg"
-                        onPress={handleRegister}
-                        backgroundColor="$blue600"
-                        rounded="$2xl"
-                    >
-                        <ButtonText color="$white">Sign Up</ButtonText>
-                    </Button>
-
-                </VStack>
-                <VStack space="md" w="90%" alignItems="center" p="$8">
-                    <TouchableOpacity onPress={() => router.back()}>
-                        <Text size="sm" color="$blue600">
-                            Already have an account?{" "}
-                            <Text fontWeight="bold">Sign In</Text>
+            <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+                <Box flex={1} justifyContent="center" alignItems="center" p="$5">
+                    <VStack space="md" w="90%" alignItems="flex-start">
+                        <Text size="3xl" fontWeight="bold" color="$black">
+                            Sign Up
                         </Text>
-                    </TouchableOpacity>
-                </VStack>
-            </Box>
+                        <Text size="md" color="$coolGray500">
+                            Start Your Journey.
+                        </Text>
+
+                        <Text size="md" color="$black">User Name</Text>
+                        <Input w="100%" size="lg" rounded="$2xl">
+                            <InputField
+                                placeholder="User name"
+                                value={username}
+                                onChangeText={setUsername}
+                            />
+                        </Input>
+
+                        <Text size="md" color="$black">Full Name</Text>
+                        <Input w="100%" size="lg" rounded="$2xl">
+                            <InputField
+                                placeholder="Full name"
+                                value={fullName}
+                                onChangeText={setFullName}
+                            />
+                        </Input>
+
+                        <Text size="md" color="$black">Email address</Text>
+                        <Input w="100%" size="lg" rounded="$2xl">
+                            <InputField
+                                placeholder="Email"
+                                keyboardType="email-address"
+                                value={email}
+                                onChangeText={setEmail}
+                            />
+                        </Input>
+
+                        <Text size="md" color="$black">Password</Text>
+                        <Input w="100%" size="lg" rounded="$2xl">
+                            <InputField
+                                placeholder="Password"
+                                secureTextEntry={!showPassword}
+                                value={password}
+                                onChangeText={setPassword}
+                                style={{ paddingRight: 40 }} // space for icon
+                            />
+                            <TouchableOpacity
+                                style={styles.icon}
+                                onPress={() => setShowPassword(!showPassword)}
+                            >
+                                <Ionicons
+                                    name={showPassword ? "eye-off" : "eye"}
+                                    size={24}
+                                    color="gray"
+                                />
+                            </TouchableOpacity>
+                        </Input>
+
+                        <Button
+                            w="100%"
+                            size="lg"
+                            onPress={handleRegister}
+                            rounded="$2xl"
+                        >
+                            <ButtonText color="$white">Sign Up</ButtonText>
+                        </Button>
+                    </VStack>
+
+                    <VStack space="md" w="90%" alignItems="center" p="$8">
+                        <TouchableOpacity onPress={() => router.back()}>
+                            <Text size="sm" color="$blue600">
+                                Already have an account?{" "}
+                                <Text fontWeight="bold">Sign In</Text>
+                            </Text>
+                        </TouchableOpacity>
+                    </VStack>
+                </Box>
+            </TouchableWithoutFeedback>
         </KeyboardAvoidingView>
     );
 }
+
+const styles = StyleSheet.create({
+    icon: {
+        position: "absolute",
+        right: 15,
+        top: "50%",
+        transform: [{ translateY: -12 }],
+    },
+});
